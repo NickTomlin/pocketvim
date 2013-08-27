3b4475c94f44369caf8d4d1874049eab066220ea
Nick Tomlin <nick.tomlin@gmail.com>
2013-08-27 08:32:58 -0500

    remove old test directory

--- test/ace_test.js
+++
@@ -1,8 +0,0 @@
-module('basic ace functionality', {
-  setup: function () {
-    console.log("i'm tired");
-  }
-})
-test('is ace loaded?', function (){
-  ok( typeof ace !== 'undefined', 'go to bed');
-})
