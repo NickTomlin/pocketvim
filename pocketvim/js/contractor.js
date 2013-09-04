93ba3b339175d5b02a8b89269109450b7b1be7f7
Nick Tomlin <nick.tomlin@gmail.com>
2013-09-04 14:00:09 -0500

    attempt at loading codemirror programatically

--- pocketvim/js/contractor.js
+++ pocketvim/js/contractor.js
@@ -1,13 +1,42 @@
 requirejs.config(requirejsConfig);
 requirejs(['lib/underscore', 'config', 'util/crxload'],
   function(_,  config, crxload) {
-    var currentDomain = window.location.host;
+  var currentDomain = window.location.host;
 
-    // perhaps we could embed a simple script to check for 'ace' or 'codemirror' on window?
-    var editor = typeof config.domains[currentDomain] != 'undefined' ? config.domains[currentDomain] : config.defaultEditor; // defaults to codemirror
+  function loadEditor (editorName) {
     require(['codemirror/edit'], function (editor){
       editor.loadDependencies(function(){
         console.log('deps loaded');
       });
     });
+  }
+
+  // perhaps we could embed a simple script to check for 'ace' or 'codemirror' on window?
+  // var editor = typeof config.domains[currentDomain] != 'undefined' ? config.domains[currentDomain] : config.defaultEditor; // defaults to codemirror
+
+  // check for existence of editor (and ideally editor version)
+  // https://developer.chrome.com/extensions/content_scripts.html#host-page-communication
+  window.addEventListener("message", function(event) {
+      // We only accept messages from ourselves
+      if (event.source != window)
+        return;
+
+      if (event.data.type && (event.data.type == "DOMSPY")) {
+        var editor = JSON.parse(event.data.text);
+
+        if (editor.name) {
+          // this needs to be a constructed path
+          require(['editor'], function (Editor){
+            var dependencies = ['js/keybindings/codemirror/vim.js', 'js/modules/codemirror/embed.js'];
+            cm = new Editor(dependencies, {version: editor.version });
+            cm.loadDependencies();
+          });
+        }
+      }
+
+  }, false);
+
+  // kick things off
+  crxload('js/modules/util/domspy.js');
 });
+
