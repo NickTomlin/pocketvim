b193c75a219fc5fcddee0de989c0f35398c6c3bc
Nick Tomlin <nick.tomlin@gmail.com>
2013-09-10 09:19:32 -0500

    switch to CDNJS for vim binding hosting

--- pocketvim/js/modules/ace/embed.js
+++ pocketvim/js/modules/ace/embed.js
@@ -1,6 +1,5 @@
-// assumes window.ace exists
-// right now, this request is only going to work on github
-ace.require("ace/lib/net").loadScript("https://rawgithub.com/ajaxorg/ace-builds/master/src-min-noconflict/keybinding-vim.js",
+// since chrome.extension is not availalbe on window (and are embedding script, we load bindings from CDN)
+ace.require("ace/lib/net").loadScript('//cdnjs.cloudflare.com/ajax/libs/ace/1.1.01/keybinding-vim.js',
 function() {
     e = document.querySelector(".ace_editor").env.editor; // github also adds a class of .ace-github to ace instance
     e.setKeyboardHandler(ace.require("ace/keyboard/vim").handler);
