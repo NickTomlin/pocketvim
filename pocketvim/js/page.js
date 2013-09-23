24258baa32407829d06ee2898d92125e8a5fcc13
Nick Tomlin <nick.tomlin@gmail.com>
2013-09-23 14:52:57 -0500

    remove duplicate injectScript function

--- pocketvim/js/page.js
+++ pocketvim/js/page.js
@@ -16,20 +16,6 @@ function injectScript (url, callback) {
     (document.head||document.documentElement).appendChild(s);
 }
 
-/**
-* Load "in-dom" js resources to get around
-* Chrome's seperation of extension / page DOM.
-* @param  {string} url path to js file from extension root
-* @param  {function} callback  optional callback to be fired on script 'onload' event
-*/
-function crxload (url, callback) {
-  var s = document.createElement('script');
-  s.src = chrome.extension.getURL(url);
-  // @todo potentially remove parentNode here and then call callback?
-  s.onload = callback;
-  (document.head||document.documentElement).appendChild(s);
-}
-
 /* ==========================================================================
    Editor
    ========================================================================== */
