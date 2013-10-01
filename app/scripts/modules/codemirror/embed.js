0b71381227a4bb3c4da5d1113bddc54cecaed82b
Nick Tomlin <nick.tomlin@gmail.com>
2013-10-01 14:38:56 -0500

    add readystate checks to injected scripts

--- app/scripts/modules/codemirror/embed.js
+++ app/scripts/modules/codemirror/embed.js
@@ -1,5 +1,11 @@
-// @todo: we need a way of passing this selector based on domain
-var mirrors = document.querySelectorAll('.CodeMirror');
-Array.prototype.forEach.call(mirrors, function (instance){
- instance.CodeMirror.setOption('keyMap','vim');
-});
+var PocketVim = PocketVim || {};
+
+PocketVim.embedCM = function () {
+  // @todo: we need a way of passing this selector based on domain
+  var mirrors = document.querySelectorAll('.CodeMirror');
+  Array.prototype.forEach.call(mirrors, function (instance){
+   instance.CodeMirror.setOption('keyMap','vim');
+  });
+}
+
+PocketVim.embedCM();
