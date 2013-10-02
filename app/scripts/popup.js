3115fc0ae97787d7541da0c589a3c3624f787702
Nick Tomlin <nick.tomlin@gmail.com>
2013-10-02 09:21:09 -0500

    trim whitespace on popup input. resolves #5

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
   var submit = document.querySelector('button');
   submit.addEventListener('click', function () {
     // there is probably a more elegant way to do this...
-    proposedUrl = input.value;
+    proposedUrl = input.value.trim(); // chrome supports string.trim ^^
     currentUrls = options('enabled_urls') || '';
     if (currentUrls.indexOf(proposedUrl) === -1) {
       options('enabled_urls', currentUrls + proposedUrl + '\n' );
