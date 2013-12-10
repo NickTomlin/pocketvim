27e7f5fe433bb8c385bbc887acf63cd5fc5c58fc
Nick MBA <nick.tomlin@gmail.com>
2013-12-10 14:03:15 -0600

    properly testing isEnabled

---
+++ test/lib/chrome_helper.js
@@ -0,0 +1,9 @@
+// "mock" some chrome api methods
+var chrome = {
+    extension: {
+        // onMessage.addListener
+        onMessage: {
+            addListener: function () {}
+        }
+    }
+}
--- test/spec/main.js
+++ test/spec/main.js
@@ -1,11 +1,46 @@
-/*global describe, it */
+/*global describe, it, chrome, options */
 'use strict';
+
 var assert = chai.assert;
+
+// mock "options"
+var options = function (option) {
+    var opts = {
+        "foo": "bar",
+        "enabled_urls": [
+           'http://jsbin.com/*',
+           'http://jsfiddle.net/*',
+           'http://www.google.com',
+           'http://www.google.com/true'
+        ].join('\n')
+    }
+    return opts[option];
+};
+
 (function () {
     describe('Test isEnabled', function () {
-        describe('sanity', function () {
-            it('is function defined', function () {
-                assert(typeof isEnabled === 'function', 'is enabled is not defined');
+        describe('test our mocks', function () {
+            it('should use our copy of options', function (){
+                assert(options("foo") === "bar", 'mock options is not in scope ')
+            })
+        })
+        describe('check urls', function () {
+            it('is a function', function () {
+                assert(typeof isEnabled === 'function', 'isEnabled is not a function');
+            });
+
+            it('url regex', function () {
+                assert(isEnabled({url: 'http://foo.com/bar'}) === false, 'failed to check non enabled url');
+                // why isn't this working?
+                assert(isEnabled({url: 'http://www.google.com/false'}) === false, 'false positive non wildcard');
+                assert(isEnabled({url: 'http://www.google.com/true' }) === true, 'failed non wildcard path');
+                assert(isEnabled({url: 'http://www.google.com'}) === true, 'failed exact path');
+            });
+
+            it('wildcards', function () {
+                assert(isEnabled({url: 'http://jsbin.com/foo/bar'}) === true, 'failed to enable  jsbin');
+                assert(isEnabled({url: 'http://jsfiddle.net/nV3nF/'}) === true, 'failed to enable jsfiddle');
+                assert(isEnabled({url: 'http://foo.net/nV3nF/'}) === false, 'falsely enabled wildcard');
             });
         });
     });
