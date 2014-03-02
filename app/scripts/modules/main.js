88224a3c0dd0d5ac1d97c20484b740069a92a1ff
Nick Tomlin <nick.tomlin@gmail.com>
2014-03-02 16:59:33 -0600

    Cleanup main.js module and add comments to isEnabled

--- app/scripts/modules/main.js
+++ app/scripts/modules/main.js
@@ -23,7 +23,6 @@ define(function (require, exports, module) {
   // our event bus
   // inspired by Vimium
   module.exports.addOnMessageListener = function () {
-    console.log('listeners');
     chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
       if (publicApi[request.method]) {
         sendResponse(publicApi[request.method](request));
@@ -93,11 +92,15 @@ define(function (require, exports, module) {
   };
 
   // inspired / stolen from Vimium
+  /**
+   * Check to see if given object has a url that is in our enabled urls list
+   * @param request {object} object with a url property
+   * @todo remove the depedency on request.url and have param be a string.
+   */
   isEnabled = module.exports.isEnabled = function (request) {
     var url = request.url;
     var enabledUrls = options("enabled_urls");
     var enabled = false;
-
     // handle wildcard in enabled urls
     // use array.some so we can escape http://stackoverflow.com/a/2641374/1048479
     // @todo#codingsmell is this too clever?
