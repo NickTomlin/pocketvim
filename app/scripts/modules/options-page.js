8edb1b05a9791e4b54be4028e064764cbf487eb2
Nick Tomlin <nick.tomlin@gmail.com>
2014-03-05 18:15:42 -0600

    Add passing options-page tests. Refactor slightly

--- app/scripts/modules/options-page.js
+++ app/scripts/modules/options-page.js
@@ -5,92 +5,93 @@ define(function (require, exports, module) {
 
   var root;
 
-// our option inputs
-var settingInputs = ['enabled_urls'];
+  // our option inputs
+  var settingInputs = ['enabled_urls'];
 
-function getInput (name) {
-  return root.querySelector('[name="'+ name + '"]');
-}
+  function getInput (name) {
+    return root.querySelector('[name="'+ name + '"]');
+  }
 
-function getInputValue(ele) {
-  var name = ele.nodeName.toLowerCase();
-  var inputs = {
-    'select': function () {
-      return this.children[this.selectedIndex].value;
-    },
-    'input': function () {
-      return this.value;
-    },
-    'textarea': function () {
-      var cleanedValues = this.value
+  function getInputValue(ele) {
+    var name = ele.nodeName.toLowerCase();
+    var inputs = {
+      'select': function () {
+        return this.children[this.selectedIndex].value;
+      },
+      'input': function () {
+        return this.value;
+      },
+      'textarea': function () {
+        this.value = this.value
                             .split('\n')
                             .filter(function(value){
                               return value !== "";
                             })
                             .join('\n') + '\n';
-      this.value = cleanedValues;
-      return this.value; // we are just storing things as whole lines
-    }
-  };
-  return inputs[name].call(ele);
-}
-
-
-// @todo#cleanup run trim to remove empty strings?
-function save_options() {
-  for (var i = 0; i < settingInputs.length; i++) {
-    var inputName = settingInputs[i];
-    var input = getInput(inputName);
-    options(inputName, getInputValue(input));
-  }
-  // Update status to let user know options were saved.
-  var status = document.getElementById('status');
-  status.innerHTML = 'Options Saved.';
-  setTimeout(function() {
-    status.innerHTML = '';
-  }, 750);
-}
-// default is initialy 'undefined', we set it to false when we retoreDefaults()
-
-/**
- * Restore options from localstorage
- * @param  {bool} reset reset options to defaults.
- */
-function restoreOptions() {
-
-  for (var i = 0; i < settingInputs.length; i++) {
-    var key = settingInputs[i];
-    var value = options(key);
-
-    if (!value) {
-     continue;
-    }
-    var settingElement = getInput(key);
-    var type = settingElement.nodeName.toLowerCase();
-
-    if (type === 'select') {
-      for (var j = 0; j < settingElement.children.length; j++) {
-        var child = settingElement.children[j];
-        if (child.value == value) {
-          child.selected = "true";
-        }
+        return this.value; // we are just storing things as whole lines
       }
-    } else if (type === 'textarea') {
-      settingElement.value = value;
+    };
+
+    return inputs[name].call(ele);
+  }
+
+
+  // @todo#cleanup run trim to remove empty strings?
+  function save_options() {
+    for (var i = 0; i < settingInputs.length; i++) {
+      var inputName = settingInputs[i];
+      var input = getInput(inputName);
+      options(inputName, getInputValue(input));
+    }
+
+    // Update status to let user know options were saved.
+    var status = root.querySelector('#status');
+    status.innerHTML = 'Options Saved.';
+    setTimeout(function() {
+      status.innerHTML = '';
+    }, 750);
+  }
+
+  /**
+  * Restore options from localstorage
+  * @param  {bool} reset reset options to defaults.
+  */
+  function restoreOptions() {
+
+    for (var i = 0; i < settingInputs.length; i++) {
+      var key = settingInputs[i];
+      var value = options(key);
+
+      if (!value) {
+      continue;
+      }
+      var settingElement = getInput(key);
+
+      var type = settingElement.nodeName.toLowerCase();
+
+      if (type === 'select') {
+        for (var j = 0; j < settingElement.children.length; j++) {
+          var child = settingElement.children[j];
+          if (child.value == value) {
+            child.selected = "true";
+          }
+        }
+      } else if (type === 'textarea') {
+        settingElement.value = value;
+      }
     }
   }
-}
 
-function resetOptions() {
-  options.restoreDefaultOptions();
-  restoreOptions();
-}
+  function resetOptions() {
+    options.restoreDefaultOptions();
+    restoreOptions();
+  }
 
-module.exports.init = function (rootElement) {
-  root = rootElement;
-  restoreOptions();
-  document.querySelector('#save').addEventListener('click', save_options);
-  document.querySelector('#defaults').addEventListener('click', resetOptions);
-}
+  module.exports.init = function (rootElement) {
+    root = rootElement;
+    restoreOptions();
 
+    rootElement.querySelector('#save').addEventListener('click', save_options);
+    rootElement.querySelector('#defaults').addEventListener('click', resetOptions);
+  };
 });
---
+++ test/unit/options-page-test.js
@@ -0,0 +1,59 @@
+'use strict';
+
+define(['modules/options-page', 'test/unit/support/resetoptions'], function (optionsPage, resetOptions) {
+  // strip out non-necessary parts of view
+  var optionsPageDiv = document.createElement('div');
+  optionsPageDiv.innerHTML = __html__['app/html/options.html'];
+  optionsPageDiv.innerHTML = optionsPageDiv.querySelector('#content').innerHTML;
+
+  var fixture, select, saveButton, resetButton;
+  beforeEach(function () {
+    fixture = document.createElement('div');
+    fixture.id = 'fixture';
+    fixture.innerHTML = optionsPageDiv.innerHTML;
+
+    select = fixture.querySelector('#enabled_urls');
+    saveButton = fixture.querySelector('#save');
+    resetButton = fixture.querySelector('#defaults');
+
+    resetOptions();
+
+    optionsPage.init(fixture);
+  });
+
+  afterEach(function () {
+    fixture = null;
+  });
+
+  describe('options page', function () {
+    it('restores options', function () {
+      select.value = select.value + "\nhttp://www.foobar.com/*";
+      resetButton.click();
+
+      var urls = select.value.split('\n');
+
+      expect(urls.length).toEqual(8);
+    });
+
+    it('saves options', function () {
+      var newUrl = "\nhttp://www.foobar.com/*";
+
+      select.value = select.value + newUrl;
+      saveButton.click();
+
+      expect(localStorage.enabled_urls).toContain(newUrl);
+    });
+
+    it('initializes select area with option content', function () {
+      var urls = select.value.split('\n');
+
+      expect(urls.length).toEqual(8);
+    });
+
+    it('initializes select area with option content', function () {
+      var urls = select.value.split('\n');
+
+      expect(urls.length).toEqual(8);
+    });
+  });
+});
