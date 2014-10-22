087e57465a2b37fed8ac03f101a3b3b7c8e1c1bc
Nick Tomlin <nick.tomlin@gmail.com>
2014-10-22 17:59:07 -0500

    Cleanup indentation in main test

--- test/unit/main-test.js
+++ test/unit/main-test.js
@@ -1,30 +1,31 @@
 /*global describe, expect, it, chrome */
+
 'use strict';
 
 define(['modules/background', 'test/unit/support/resetoptions'], function (background, resetOptions) {
-    beforeEach(function () {
-      resetOptions();
+  beforeEach(function () {
+    resetOptions();
+  });
+
+  describe('#isEnabled', function () {
+    var isEnabled = background.isEnabled;
+
+    it('should not match non_enabled urls', function () {
+      expect(isEnabled({url: 'http://foo.com/bar'})).toBeFalsy();
+      expect(isEnabled({url: 'http://www.google.com/false'})).toBeFalsy();
     });
 
-    describe('#isEnabled', function () {
-            var isEnabled = background.isEnabled;
-
-            it('should not match non_enabled urls', function () {
-                expect(isEnabled({url: 'http://foo.com/bar'})).toBeFalsy();
-                expect(isEnabled({url: 'http://www.google.com/false'})).toBeFalsy();
-            });
-
-            it('should match enabled urls at root', function () {
-                expect(isEnabled({url: 'http://jsbin.com/'})).toBeTruthy();
-            });
-
-            it('should match wildcard urls', function () {
-                expect(isEnabled({url: 'http://jsbin.com/foo/bar/baz'})).toBeTruthy();
-                expect(isEnabled({url: 'http://jsbin.com/var/1'})).toBeTruthy();
-            });
-
-            it('should not match before wildcard paths', function () {
-                expect(isEnabled({url: 'http://cssdeck.com/'})).toBeFalsy();
-            });
+    it('should match enabled urls at root', function () {
+      expect(isEnabled({url: 'http://jsbin.com/'})).toBeTruthy();
     });
+
+    it('should match wildcard urls', function () {
+      expect(isEnabled({url: 'http://jsbin.com/foo/bar/baz'})).toBeTruthy();
+      expect(isEnabled({url: 'http://jsbin.com/var/1'})).toBeTruthy();
+    });
+
+    it('should not match before wildcard paths', function () {
+      expect(isEnabled({url: 'http://cssdeck.com/'})).toBeFalsy();
+    });
+  });
 });
